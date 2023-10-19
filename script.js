94b210702d7ebc6a6a2498cdc86b77384b966c2c
Azamat2004 <kokosherovazamat04@gmail.com>
2023-10-19 23:01:31 +0600

    first

---
+++ script.js
@@ -0,0 +1,36 @@
+function findLongestWord(str) {
+    let words = str.match(/[а-яА-Яa-zA-ZёЁ]{1,}/gi);
+    let theLongestWord = words[0];
+  
+    for(let i = 1 ; i < words.length ; i++) {
+      if(theLongestWord.length < words[i].length)
+      {
+        theLongestWord = words[i];
+      }
+    }
+    return theLongestWord;
+  }
+  console.log(findLongestWord('Самый быстрый футболист Эрон Уан-Биссака'));  
+
+// дополнительный
+
+function detect_data_type(value) {
+    var type = typeof value;
+    
+    if (type == 'object') {
+        if (value == null) {
+            return 'null';
+        }
+        return 'object';
+    }
+    
+    return type;
+}
+
+
+console.log(detect_data_type({}));
+console.log(detect_data_type(null));
+console.log(detect_data_type(true));
+console.log(detect_data_type(function(){}));
+console.log(detect_data_type(42));
+console.log(detect_data_type("Hello, World!"));
